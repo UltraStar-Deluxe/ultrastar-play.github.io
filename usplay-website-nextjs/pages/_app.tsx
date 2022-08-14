e037eb389132403454642043fba8036b9ecae03c
Andreas Stange <anst.dev@gmail.com>
2022-08-14 19:23:33 +0200

    installed and using local resources (e.g. Google Font stylesheets)

--- usplay-website-nextjs/pages/_app.tsx
+++ usplay-website-nextjs/pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import '../styles/google-font-Kanit.css'
+import '../styles/google-font-Mulish.css'
+import '../styles/google-font-Newsreader.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import type { AppProps } from 'next/app'
 
 // The bootstrap import has issues when document is not defined (server side rendering maybe?)
--- usplay-website-nextjs/pages/_document.tsx
+++ usplay-website-nextjs/pages/_document.tsx
@@ -10,16 +10,6 @@ export default function Document() {
         <meta name="author" content="" />
         <title>UltraStar&nbsp;Play - Free Singing Game for Desktop and Mobile</title>
         <link rel="icon" type="image/x-icon" href="favicon.ico" />
-        {/* Bootstrap icons */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        {/* Google fonts */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
-        {/* Core theme CSS (includes Bootstrap) */}
-        <link href="css/styles.css" rel="stylesheet" />
-        <link href="css/additional-styles.css" rel="stylesheet" />
     </Head>
     <body id="page-top">
         <Main />
