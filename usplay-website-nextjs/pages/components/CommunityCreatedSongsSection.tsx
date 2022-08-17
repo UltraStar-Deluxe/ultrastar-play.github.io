cce56791bc40268c735b3ee103f3265246c48c32
Andreas Stange <anst.dev@gmail.com>
2022-08-17 19:49:25 +0200

    new EasyToUseSection and minor changes

--- usplay-website-nextjs/pages/components/CommunityCreatedSongsSection.tsx
+++ usplay-website-nextjs/pages/components/CommunityCreatedSongsSection.tsx
@@ -5,7 +5,7 @@ const SingIdeaEntry = (props: {title: string, image: string}) =>
     let size = '150px';
 
     return (
-<div className="col col-md-4 d-flex flex-column justify-content-between align-items-center">
+<div className="col col-md-4 d-flex flex-column justify-content-between align-items-center pb-5">
     <div className="fs-3 text-white font-alt">{props.title}</div>
     <img className="circle-image-cropped" style={{height: size, width: size}} src={props.image} />
 </div>
@@ -21,8 +21,8 @@ const CommunityCreatedSongsSection = () => (
         <div className="container px-5">
             <div className="row justify-content-center">
                 <div className="col-xl-8">
-                    <div className="h2 fs-1 text-white font-alt">Sing your favorite songs!</div>
-                    <div className="h2 fs-3 text-white font-alt">There are thousands of community created songs in the UltraStar format on the internet.</div>
+                    <h1 className="text-white font-alt">Sing your favorite songs!</h1>
+                    <h3 className="text-white font-alt">There are thousands of community created songs in the UltraStar format on the internet.</h3>
                 </div>
             </div>
         </div>
@@ -30,11 +30,12 @@ const CommunityCreatedSongsSection = () => (
         <div className="container px-5">
             <hr className="bg-light"/>
             <div className="row">
-                <SingIdeaEntry title="Sing current top hits" image="img/record-159211_1280.png"/>
-                <SingIdeaEntry title="Sing the TV series intro from your childhood" image="img/animals-44569_1280.png"/>
-                <SingIdeaEntry title="Sing amazing game soundtracks" image="img/headphones-6636989_1920.jpg"/>
+                <SingIdeaEntry title="Sing current top hits" image="img/vinyl-2202325_1280.png"/>
+                <SingIdeaEntry title="Sing a TV series intro from your childhood" image="img/animals-44569_1280.png"/>
+                <SingIdeaEntry title="Sing amazing game soundtracks" image="img/controller-1784573.svg"/>
                 <SingIdeaEntry title="Sing your favorite Vocaloid and UTAU songs" image="img/sing-570519_1280.png"/>
                 <SingIdeaEntry title="Sing at a convention or event" image="img/halloween-6784081_1920.jpg"/>
+                <SingIdeaEntry title="Sing dialect from your region" image="img/chorus-5977187_1920-cut.jpg"/>
             </div>
         </div>
     </section>
