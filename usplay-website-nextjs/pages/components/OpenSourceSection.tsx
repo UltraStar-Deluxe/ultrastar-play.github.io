e621a3e36fd15be08665b7776b14dc986c03f772
Andreas Stange <anst.dev@gmail.com>
2022-08-18 22:18:43 +0200

    new OpenSourceSection

---
+++ usplay-website-nextjs/pages/components/OpenSourceSection.tsx
@@ -0,0 +1,37 @@
+import WaveBackground from "./WaveBackground"
+
+const OpenSourceSection = () => (
+
+<section id="open-source">
+    <div className="container px-md-5">
+        <div className="row justify-content-center">
+            <div className="col-xl-8 text-center">
+                <h1 className="font-alt"><span className="bi-gear-fill" />&nbsp;Open Source - Made with Unity&nbsp;<span className="bi-tools"/></h1>
+            </div>
+        </div>
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8">
+                <hr className="bg-light"/>
+                <p>Make this <span className="bold text-gradient">YOUR GAME</span> as well!<br />
+                    UltraStar&nbsp;Play is a free and open source project that belongs to everyone. <span className="bold text-gradient">Get involved</span>,
+                    add styles, ideas, translations, or features <span className="bold text-gradient">on GitHub</span>.</p>
+
+                <h2 className="font-alt text-center">Why Open Source?</h2>
+
+                <p>Imagine that you would buy another singing game for your current console, only to find that the vendor shuts down the online features, or you loose your song collection when changing the device.</p>
+                <p>This won't happen with UltraStar&nbsp;Play thanks to its <span className="bold text-gradient">open source nature</span>. UltraStar&nbsp;Play can <span className="bold text-gradient">evolve with the future</span> as long as it has a contributing community behind it.</p>
+
+                <h2 className="font-alt text-center">Made with Unity</h2>
+
+                <p>UltraStar Play is built on a <span className="bold text-gradient">professional game engine</span> and modern tools
+                    to provide the <span className="bold text-gradient">best experience for developers and players</span>.</p>
+                <p>More than 50% of the world's video games are made with Unity. It is safe to say that the Unity game engine is here to stay.
+                    A <span className="bold text-gradient">strong foundation</span> for UltraStar&nbsp;Play that can grow with new tools and platforms.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+)
+
+export default OpenSourceSection
