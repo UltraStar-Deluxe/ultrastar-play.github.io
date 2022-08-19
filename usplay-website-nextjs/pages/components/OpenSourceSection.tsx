b3cc8c72920d0b55a4040861e579f8a8af3a4c2b
Andreas Stange <anst.dev@gmail.com>
2022-08-19 18:34:35 +0200

    OpenSourceSection "Beyond UltraStar Play"

--- usplay-website-nextjs/pages/components/OpenSourceSection.tsx
+++ usplay-website-nextjs/pages/components/OpenSourceSection.tsx
@@ -16,17 +16,29 @@ const OpenSourceSection = () => (
                     UltraStar&nbsp;Play is a free and open source project that belongs to everyone. <span className="bold text-gradient">Get involved</span>,
                     add styles, ideas, translations, or features <span className="bold text-gradient">on GitHub</span>.</p>
 
-                <h2 className="font-alt text-center">Why Open Source?</h2>
 
-                <p>Imagine that you would buy another singing game for your current console, only to find that the vendor shuts down the online features, or you loose your song collection when changing the device.</p>
-                <p>This won't happen with UltraStar&nbsp;Play thanks to its <span className="bold text-gradient">open source nature</span>. UltraStar&nbsp;Play can <span className="bold text-gradient">evolve with the future</span> as long as it has a contributing community behind it.</p>
+                <h2 className="font-alt text-center">Why Open Source?</h2>
+                <p>Imagine that you would buy another singing game for your current console,
+                     only to find that the vendor shuts down the online features, or you loose your song collection when changing the device.</p>
+                <p>This won't happen with UltraStar&nbsp;Play because it
+                 can <span className="bold text-gradient">evolve with the future</span> as long as it has a contributing community behind it.</p>
 
                 <h2 className="font-alt text-center">Made with Unity</h2>
-
                 <p>UltraStar Play is built on a <span className="bold text-gradient">professional game engine</span> and modern tools
                     to provide the <span className="bold text-gradient">best experience for developers and players</span>.</p>
-                <p>More than 50% of the world's video games are made with Unity. It is safe to say that the Unity game engine is here to stay.
+                <p>More than 50% of the world's video games are <a href="https://unity.com/madewith">made with Unity</a>.
                     A <span className="bold text-gradient">strong foundation</span> for UltraStar&nbsp;Play that can grow with new tools and platforms.</p>
+
+
+                <h2 className="font-alt text-center">Beyond UltraStar&nbsp;Play</h2>
+                <p>Open source software does not only implement a single game or solution, it can also be the <span className="bold text-gradient">fuel for other projects</span>.</p>
+                <p>People can use UltraStar&nbsp;Play to <span className="bold text-gradient">learn on an example</span>.
+                    And it has grown its own set of libraries,
+                    which are <span className="bold text-gradient">shared with the community</span>,
+                    just like previous open source projects are <span className="bold text-gradient">empowering UltraStar&nbsp;Play</span>.</p>
+
+                <p>For example, UltraStar&nbsp;Play is mentioned in a small paper called <a href="https://ieee-cog.org/2020/papers/paper_308.pdf">Singing with an Angry-Birds-like Game</a>.
+                That's possible <span className="bold text-gradient">#OnlyWithOpenSource</span>.</p>
             </div>
         </div>
     </div>
