c12795c5e8d24dd0fe6df90706af2e2c19707e31
Andreas Stange <anst.dev@gmail.com>
2022-08-20 18:19:39 +0200

    removed assetPath and basePath. These are not needed because we have a custom domain CNAME
    - only .nojekyll was needed. Otherwise GitHubPages does not serve files inside _next folder

--- usplay-website-nextjs/next.config.js
+++ usplay-website-nextjs/next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: '/UltraStar-Deluxe/',
-    basePath: '/UltraStar-Deluxe',
     reactStrictMode: true,
     swcMinify: true,
     experimental: {
