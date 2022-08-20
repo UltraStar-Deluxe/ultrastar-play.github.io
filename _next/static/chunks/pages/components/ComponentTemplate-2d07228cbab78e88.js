4f9287f76d30c30b36344fb754767144be9bcdd6
Andreas Stange <anst.dev@gmail.com>
2022-08-20 17:15:08 +0200

    deployed static website

---
+++ _next/static/chunks/pages/components/ComponentTemplate-2d07228cbab78e88.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[248],{6466:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/ComponentTemplate",function(){return c(86)}])},86:function(a,b,c){"use strict";c.r(b);var d=c(5893),e=function(){return(0,d.jsx)("div",{})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=6466)}),_N_E=a.O()}])
