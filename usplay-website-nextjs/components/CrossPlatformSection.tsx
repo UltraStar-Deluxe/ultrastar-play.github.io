ed6379b451a36bc4ebe36f820dab9410471abe12
Andreas Stange <anst.dev@gmail.com>
2022-11-02 08:56:40 +0100

    translated CrossPlatformSection

--- usplay-website-nextjs/components/CrossPlatformSection.tsx
+++ usplay-website-nextjs/components/CrossPlatformSection.tsx
@@ -1,3 +1,4 @@
+import { T } from "./T"
 import WaveBackground from "./WaveBackground"
 
 const CrossPlatformSection = () => (
@@ -12,7 +13,7 @@ const CrossPlatformSection = () => (
     <div className="container px-md-5">
         <div className="row justify-content-center">
             <div className="col-xl-8 text-center">
-                <h1 className="display-3 text-white font-alt">Choose Your Platform</h1>
+                <h1 className="display-3 text-white font-alt"><T i18nKey="crossPlatformSection_title" /></h1>
                 <h3 className="text-white font-alt">Windows, MacOS, Linux, Android, iOS</h3>
             </div>
         </div>
@@ -24,21 +25,17 @@ const CrossPlatformSection = () => (
                      style={{maxWidth: "100%", maxHeight: "100%", width: "600px", height: "100%"}}/>
             </div>
             <div className="col-12 col-md-6 text-white lead">
-                <p>UltraStar&nbsp;Play is a cross-platform application.
-                    You can run it on all major <span className="emphasize">DESKTOP AND MOBILE</span> platforms.</p>
+                <p><T i18nKey="crossPlatformSection_p_1" /></p>
 
                 <hr className="bg-light mx-5"/>
 
-                <p>There are so many different platforms today for gaming, most singing games only focus on one.</p>
-                <p>Thus, changing platforms often means losing a song collection.
-                    Sometimes you even have to buy extra microphones for a particular platform or game.</p>
+                <p><T i18nKey="crossPlatformSection_p_2" /></p>
+                <p><T i18nKey="crossPlatformSection_p_3" /></p>
                 
                 <hr className="bg-light mx-5"/>
 
-                <p>Fortunately, UltraStar&nbsp;Play <span className="emphasize">SOLVES&nbsp;THIS</span> because
-                    it can evolve with your music taste and todays plethora of devices.</p>
-                <p>Even better - you can <span className="emphasize">MIX PLATFORMS</span>.
-                    For example, start the main game on an iOS tablet and connect an Android device with the Companion&nbsp;App</p>
+                <p><T i18nKey="crossPlatformSection_p_4" /></p>
+                <p><T i18nKey="crossPlatformSection_p_5" /></p>
             </div>
         </div>
     </div>
